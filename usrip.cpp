2064784e462740bfccfc46c63cad1a471ba36f7d
Josiah Wintermute <wintermutejosiah18@gmail.com>
2018-06-06 17:01:10 -0400

    User input

---
+++ usrip.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+    cout << "Enter your name: " << flush;
+    string input;
+    cin >> input;
+    cout << "Your name: " << input << endl;
+    
+    cout <<  "Enter a number: " << flush;
+    int num;
+    cin >> num;
+    cout << "Your number: " << num << endl;
+
+return 0;
+}
