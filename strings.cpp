6dcf32a356f17f3b9573364555174203993442d3
Josiah Wintermute <wintermutejosiah18@gmail.com>
2018-06-06 16:58:25 -0400

    String usage

---
+++ strings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+    string text = "3 is a good number ";
+    string text2 = "6 is another good one";
+    cout << text << text2  << endl;
+
+    string text3 = text + text2;
+    cout << text3 << endl;
+    return 0;
+
+}
